b2b3a376f8db83caa10c71dd9a5ca35133a36120
EmilyP27 <emily.picon@ucr.ac.cr>
2025-10-17 14:48:52 -0600

    Primer commit

---
+++ src/main/java/main/ParchisJuego.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main;
+import view.GUIInicio;
+
+/**
+ *
+ * @author TI-IN
+ */
+public class ParchisJuego {
+
+    public static void main(String[] args) {
+        GUIInicio v1 = new GUIInicio();
+        v1.setVisible(true);
+        v1.setLocationRelativeTo(null);
+    }
+}
